d7a9220662dc23de81ca52d3885e3953528d0a75
papuch <papuch@75512c87-d7f6-0310-a1d9-daee84810a20>
2005-05-12 10:30:34 +0000

    Premiers packages et premiere classe main

---
+++ src/fr/umlv/ir2/jhoover/JHoover.java
@@ -0,0 +1,11 @@
+package fr.umlv.ir2.jhoover;
+
+public class JHoover {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Bienvenue dans jHoover");
+	}
+}
