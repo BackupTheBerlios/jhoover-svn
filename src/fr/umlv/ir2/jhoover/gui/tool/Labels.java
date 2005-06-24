4f9b07142804d5596e0bb8c92402068882abc716
papuch <papuch@75512c87-d7f6-0310-a1d9-daee84810a20>
2005-06-24 11:54:55 +0000

    1ere Version graphique (architecture)

---
+++ src/fr/umlv/ir2/jhoover/gui/tool/Labels.java
@@ -0,0 +1,15 @@
+
+package fr.umlv.ir2.jhoover.gui.tool;
+
+import javax.swing.Icon;
+
+
+public final class Labels
+{
+	public static final String QUIT_LABEL = "Quit";
+	public static final String HELP_LABEL = "Help";
+	public static final String PREFERENCES_LABEL = "Preferences";
+	public static final String MENUFILE_LABEL = "File";
+	public static final String MENUEDIT_LABEL = "Edit";
+	public static final String CONFIG_LABEL = "Configuration";
+}
